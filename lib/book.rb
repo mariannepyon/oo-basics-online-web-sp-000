8e2d474e1df3316132ff697ed88fccb356afdf65
Marianne Pyon <mariannepyon@gmail.com>
2019-08-13 18:37:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,4 +8,16 @@ class Book
     @title
   end
 
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
 end
